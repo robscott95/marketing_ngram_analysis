d97dec83f9d76284a9a042e77961856ba9051f28
Robert Kuśka <r.kuska@ladder.io>
2019-05-01 20:56:51 +0200

    Updated the clean_input_data docstring and now it also checks for incorrect input file format

--- ngram_analysis.py
+++ ngram_analysis.py
@@ -3,6 +3,7 @@ Script for performing a range n-gram split on passed data.
 
 TODO:
 * Clear input text out of stop signs, with respect of dynamic keyword insertation (DKI) and emoji support.
+** DKI in Google will require an additionall step, where anything between curly braces {} should have their spaces removed.
 * Range n-gram splitting
 * Create a post-processing DataFrame containing the original dataframe with new columns containing ngrams
 * Create n-gram based DataFrames which sums everything up with respect towards the string based columns.
@@ -19,7 +20,28 @@ import argparse
 
 
 def clean_input_data(input_data_df):
+    '''
+    Helper function for cleaning the main text column (by default it's the first one).
+    Deletes stop characters and in the event of Dynamic Keyword Insertations removes spaces
+    between the words inside of curly braces.
+
+    Args:
+        - input_data_df (DataFrame): The DF in which the first column contains the text that
+            will be tokenized.
     
+    Returns:
+        - DataFrame: Modified `input_data_df` which has been approprietly pre-processed for 
+            further data analysis.
+
+    Raises:
+        - TypeError: When the first column of the DataFrame isn't an object.
+    '''
+    
+    if not input_data_df.iloc[0].dtype == 'O':
+        raise TypeError(f"The first column of the input file is not text based.")
+
+    stop_characters = ".,:;?!()"
+
     
     return input_data_df
 
