20dbd118c0f3d5e336cb904321c2b42d60926f1a
Robert Kuśka <r.kuska@ladder.io>
2019-05-02 15:41:02 +0200

    clean_input_data returns an upgraded input_data_df with approprietly cleaned text

--- ngram_analysis.py
+++ ngram_analysis.py
@@ -3,7 +3,8 @@ Script for performing a range n-gram split on passed data.
 
 TODO:
 * Clear input text out of stop signs, with respect of dynamic keyword insertation (DKI) and emoji support.
-** DKI in Google will require an additionall step, where anything between curly braces {} should have their spaces removed.
+** DKI in Google will require an additionall step, where anything between curly braces {} should have their 
+   spaces removed.
 * Range n-gram splitting
 * Create a post-processing DataFrame containing the original dataframe with new columns containing ngrams
 * Create n-gram based DataFrames which sums everything up with respect towards the string based columns.
@@ -17,7 +18,9 @@ TODO:
 import pandas as pd
 import nltk
 import argparse
+import re
 
+pd.options.mode.chained_assignment = None
 
 def clean_input_data(input_data_df):
     '''
@@ -30,19 +33,47 @@ def clean_input_data(input_data_df):
             will be tokenized.
     
     Returns:
-        - DataFrame: Modified `input_data_df` which has been approprietly pre-processed for 
-            further data analysis.
+        - DataFrame: Modified `input_data_df` which has now an additional column `cleaned_text`
+            which contains the processed and cleaned text.
 
     Raises:
         - TypeError: When the first column of the DataFrame isn't an object.
     '''
+
+    def delete_spaces_in_substring(s, pat=r'{.*?}'):
+        '''Helper function for removing spaces in a substring of a string'''
+        matches = re.findall(pat, s)
+        
+        if matches:
+            for match in matches:
+                match_replacement = re.sub(r'\s+', '', match)
+                s = re.sub(match, match_replacement, s)
+
+        return s
     
     if not input_data_df.iloc[0].dtype == 'O':
         raise TypeError(f"The first column of the input file is not text based.")
 
-    stop_characters = ".,:;?!()"
+    input_data_df['cleaned_text'] = input_data_df.iloc[:,0]
 
-    
+    input_data_df = input_data_df[pd.notnull(input_data_df.iloc[:,0])]
+
+    # Leave out curly braces and | sign which denotes DKI and the end of
+    # the headline/description in AdWords.
+    # We just want to remove the most popular punctuation to remove redundant
+    # duplicate ngrams while also want to have an insight into how less
+    # common characters influence the analysis.
+    stop_characters = ".,:;?!()\\n\""
+
+    input_data_df['cleaned_text'] = input_data_df['cleaned_text'].str.lower()
+    input_data_df['cleaned_text'].replace({f"[{stop_characters}]": ' '}, inplace=True, regex=True)
+   
+    # Fix with numerical data - now it's all togheter instead of random 3 digit ngrams.
+    input_data_df['cleaned_text'].replace({r'(\d)\s(\d)': r'\1\2'}, inplace=True, regex=True) 
+    input_data_df['cleaned_text'] = input_data_df['cleaned_text'].apply(lambda s: delete_spaces_in_substring(s) 
+                                                                        if '{' in s else s)
+    input_data_df['cleaned_text'].replace({r'\s+': ' '}, inplace=True, regex=True) 
+
     return input_data_df
 
 ###################
